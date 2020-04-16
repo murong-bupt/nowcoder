f784da25642902de5bb45d8977d11cfda307d747
murong <duoyi_xie@163.com>
2020-04-16 20:36:34 +0800

    初始化

---
+++ src/main/java/com/murong/nowcoder/NowcoderApplication.java
@@ -0,0 +1,16 @@
+package com.murong.nowcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xdy
+ */
+@SpringBootApplication
+public class NowcoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NowcoderApplication.class, args);
+    }
+
+}
