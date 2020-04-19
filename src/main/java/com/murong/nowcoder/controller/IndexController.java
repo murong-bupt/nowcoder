1e17df311374c4602a2b4b9617d0ed845c407f99
murong <duoyi_xie@163.com>
2020-04-19 13:50:14 +0800

    request

--- src/main/java/com/murong/nowcoder/controller/IndexController.java
+++ src/main/java/com/murong/nowcoder/controller/IndexController.java
@@ -1,8 +1,16 @@
 package com.murong.nowcoder.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
 
 /**
  * @author xdy
@@ -29,9 +37,74 @@ public class IndexController {
                 "%s组的%d号用户，type为%d,key为%s",groupId,userId,type,key);
     }
 
+    /**
+     * 返回模板
+     */
     @RequestMapping(value = "/vm",method = RequestMethod.GET)
-    public String template(){
+    public String template(Model model){
+        model.addAttribute("value1","v1");
         return "home.html";
     }
 
+    /**
+     * Request\Response
+     */
+    @RequestMapping(path = {"/request"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public String request(Model model, HttpServletResponse response,
+                          HttpServletRequest request,
+                          HttpSession httpSession,
+                          @CookieValue("JSESSIONID") String sessionId) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("COOKIEVALUE:" + sessionId);
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            sb.append(name + ":" + request.getHeader(name) + "<br>");
+        }
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                sb.append("Cookie:" + cookie.getName() + " value:" + cookie.getValue());
+            }
+        }
+        sb.append(request.getMethod() + "<br>");
+        sb.append(request.getQueryString() + "<br>");
+        sb.append(request.getPathInfo() + "<br>");
+        sb.append(request.getRequestURI() + "<br>");
+
+        response.addHeader("nowcoderId", "hello");
+        response.addCookie(new Cookie("username", "nowcoder"));
+
+        return sb.toString();
+    }
+    /**
+     * 重定向
+     */
+    @RequestMapping(path = {"/redirect/{code}"}, method = {RequestMethod.GET})
+    public RedirectView redirect(@PathVariable("code") int code,
+                                 HttpSession httpSession) {
+        httpSession.setAttribute("msg", "jump from redirect");
+        RedirectView red = new RedirectView("/", true);
+        if (code == 301) {
+            red.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        }
+        return  red;
+    }
+
+    @RequestMapping(path = {"/admin"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public String admin(@RequestParam("key") String key) {
+        if ("admin".equals(key)) {
+            return "hello admin";
+        }
+        throw  new IllegalArgumentException("参数不对");
+    }
+    /**
+     * Error
+     */
+    @ExceptionHandler()
+    @ResponseBody
+    public String error(Exception e) {
+        return "error:" + e.getMessage();
+    }
 }
